58a9c1562394d75c56b43448d7ea3e72abee34e9
Marcus Reinhardt <webstone@gmail.com>
2019-09-07 12:27:20 +0200

    WIP

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,6 +1,7 @@
 export default function (Vue, {
     head
 }) {
+    console.log("plugin-remark-embed client called");
     head.script.push({
         src: 'https://www.example.com/my-script.js'
     })
