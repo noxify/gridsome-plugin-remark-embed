c5146a07ce4c2da1fb0bd5b747f73970590f2ac8
Marcus Reinhardt <webstone@gmail.com>
2019-09-09 22:34:15 +0200

    Giphy Provider: Bugfix

--- src/providers/Giphy.js
+++ src/providers/Giphy.js
@@ -4,12 +4,19 @@ const _ = require('lodash');
 class Giphy extends Provider {
     constructor(options) {
         super(options);
-        this.regexp = /https?:\/\/(\?|media\.giphy\.com\/media\/([^ /\n]+)\/giphy\.gif|i\.giphy\.com\/([^ /\n]+)\.gif|giphy\.com\/gifs\/(?:.*-)?([^ /\n]+))/i;
+        this.regexp = /https?:\/\/(?:\?|media\.giphy\.com\/media\/([^ /\n]+)\/giphy\.gif|i\.giphy\.com\/(?:[^ /\n]+)\.gif|giphy\.com\/gifs\/(?:.*-)?([^ /\n]+))/i;
         this.template = __dirname + '/../templates/Giphy.hbs';
         this.idPosition = 1;
         this.options = _.defaults(options, {
             responsive: true
         });
+    }
+
+    getEmbedId(url) {
+        const res = url.match(this.regexp);
+        if(!res) return false;
+        const regExpPosition = (res[this.idPosition].indexOf('http') !== -1) ? 2 : this.idPosition
+        return res[regExpPosition];
     }
 }
 
